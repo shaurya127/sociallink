a53315e29c5e3a9d12bbcc60a0fafdd2326dbf38
shaurya127 <shaurya1272000@gmail.com>
2022-07-14 12:47:09 +0530

    indicator

--- screens/Spalsh_Screen.js
+++ screens/Spalsh_Screen.js
@@ -1,19 +1,47 @@
 import React ,{useEffect} from 'react';
-import { StyleSheet, Text, View,Image } from 'react-native';
-import SplashScreen from "../screens/images/SplashScreen.png";
+import { StyleSheet, Text, View,Image,ImageBackground,StatusBar,SafeAreaView } from 'react-native';
+import SplashScreen from "../screens/images/SplashScreen.jpg";
 const Splash_Screen = ({navigation}) => {
   useEffect(() => {
     setTimeout(() => {
       navigation.navigate('username');
-    }, 1000);
+    }, 3000);
   }, []);
 
   return (
-    <View >
-      <Text>Splash Screen</Text>
-    </View>
-  );
+    <SafeAreaView style={styles.container}>
+        <StatusBar backgroundColor="#000" barStyle="light-content" />
+        <ImageBackground source={SplashScreen} style={styles.image}>
+       
+        </ImageBackground>
+    </SafeAreaView>
+    );
+   
 };
+
+
+
 export default Splash_Screen;
+
+// add styles
+const styles = StyleSheet.create({
+    image: {
+        width: '100%',
+        
+        height: '100%',
+        resizeMode: 'contain',
+    },
+    container: {
+        flex: 1,
+        backgroundColor: '#000',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+
+            
+
+}
+);
 
 
